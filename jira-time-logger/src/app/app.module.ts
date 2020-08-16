53c7fe490796b5316f60ae5279447096a4513aa3
Viknesh Ramm <vikneshramm@outlook.com>
2020-08-16 22:03:42 +0530

    ftr(dummy-server): created a Express server with API to return current time
    Signed-off-by: Viknesh Ramm <vikneshramm@outlook.com>

--- jira-time-logger/src/app/app.module.ts
+++ jira-time-logger/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
@@ -12,7 +13,7 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- jira-time-logger/src/app/footer/footer.component.ts
+++ jira-time-logger/src/app/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { version } from '../../../package.json';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-footer',
@@ -8,12 +9,23 @@ import { version } from '../../../package.json';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   appVersion: string;
+  dataFromServer: string;
 
   ngOnInit() {
     this.appVersion = version;
+    this.dataFromServer = '';
+    this.getDataFromServer();
+  }
+
+  getDataFromServer() {
+    this.http.get<{time: string}>('http://localhost:3000/api/time').subscribe((response) => {
+      this.dataFromServer = response.time;
+    }, (failureResponse) => {
+      console.log('Request Filed');
+    });
   }
 
 }
